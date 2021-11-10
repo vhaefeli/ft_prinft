444936b34136522a65f2ab41b8cda89a8b5e0632
Vanessa Haefeli <vhaefeli@student.42lausanne.ch>
2021-11-10 20:48:41 +0100

    on s'approche du but 2

--- ft_processargprintf.c
+++ ft_processargprintf.c
@@ -6,24 +6,21 @@
 /*   By: vhaefeli <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 17:38:09 by vhaefeli          #+#    #+#             */
-/*   Updated: 2021/11/10 17:54:40 by vhaefeli         ###   ########.fr       */
+/*   Updated: 2021/11/10 20:47:28 by vhaefeli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "libftprintf.h"
 
-static
-static bool	checkiftype(char input)
+static bool	checktype(char const *input, char *set, int j, int const nbflag)
 {
-	char const *set;
 	int 		i;
 
 	i = 0;
-	set = " -+";
-	while (set[i] != '\0')
+	while (set[i] != '\0' && j <= nbflag)
 	{
-		if (input[0] = set[i])
+		if (input[j + i] = set[i])
 			return (true);
 		else
 			i++;
@@ -31,17 +28,44 @@ static bool	checkiftype(char input)
 	return (false);
 }
 
+static char *ft_processflagbefore(char const *input, int j, int const nbflag, int addsize)
+{
+	char	*beforestr;
+
+	return (beforestr);
+}
+
+static char *ft_processflagafter(char const *input, int j, int const nbflag, int addsize);
+{
+	char 	*afterstr;
+
+	return (afterstr);
+}
+
 int ft_processargprintf(char const *input, va_list args, int j, int const nbflag)
 {
 	int 	addsize;
 	int		i;
 	char	*str;
+	char	*beforestr;
+	char	*afterstr;
 
 	i = j + nbflag;
-	
-
-	
-
-
+	if (checktype(input, ".", j, nbflag))
+	{
+		str = ft_strprecition(input, args, j, nbflag);
+		return (ft_strlen(str));
+	}
+	str = ft_processargtype(input[i], args);
+	addsize = ft_strlen(str);
+	beforestr = ft_processflagbefore(input, j, nbflag, addsize);
+	afterstr = ft_processflagafter(input, j, nbflag, addsize);
+	ft_putstr_fd(beforestr, 1);
+	ft_putstr_fd(str, 1);
+	ft_putstr_fd(afterstr, 1);
+	addsize += ft_strlen(beforestr) + ft_strlen(afterstr);
+	free (str);
+	free(beforestr);
+	free(afterstr);
 	return (addsize);
 }
